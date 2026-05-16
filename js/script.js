fb74d7bf1201752318f6514a997e878e40786e52
acms2345 <ancamasi.2345@gmail.com>
2026-05-16 12:59:29 -0300

    Adiciona botão de repositório com ícone do GitHub no rodapé

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Site carregado!');
+});
+
+document.querySelector('.botao_repositorio').addEventListener('click', function() {
+    window.location.href = 'https://github.com/acms2345/acms2345.github.io';
+});
